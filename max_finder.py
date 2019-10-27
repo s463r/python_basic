ce604a97d71fdfee01d6a15e079cdba2e99f9cc9
saber <saber.ahmed@g.bracu.ac.bd>
2019-10-28 00:38:33 +0100

    I have make the desired code. Next thing is too finding out the data type. If it is a integer or a number then the loop will continue and if it is other than a number the the while loop will be vanished.

--- max_finder.py
+++ max_finder.py
@@ -1,1 +1,16 @@
-print('Hallo WOrld')
+def max_finder(numbers):
+    '''
+    :param numbers: Series of numbers that has been given input by the user
+    :return: This will only return the maximum number among the inputs
+    '''
+    return max(numbers)
+
+
+number =[]
+while(1):
+    a = input("Enter a number or to finish type E :").lower()
+    if (a=='e'):
+        break;
+    else:
+        number.append(int(a))
+print(max_finder(number))
