2360ad49091b0c409e2960404bfffce8c4a15edc
saber <saber.ahmed@g.bracu.ac.bd>
2019-10-28 09:42:57 +0100

    I have make the desired code. From my side Code is done.

--- max_finder.py
+++ max_finder.py
@@ -1,16 +1,18 @@
-def max_finder(numbers):
+def max_finder(numbers=0):
     '''
     :param numbers: Series of numbers that has been given input by the user
     :return: This will only return the maximum number among the inputs
     '''
+    if (len(number)==0):
+        return 'No number found'
     return max(numbers)
 
 
 number =[]
 while(1):
     a = input("Enter a number or to finish type E :").lower()
-    if (a=='e'):
+    if (a=='end'):
         break;
     else:
-        number.append(int(a))
+        number.append(float(a))
 print(max_finder(number))
