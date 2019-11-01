f5df963b978adb1a8adad52c743689cf0b6e8a56
saber <saber.ahmed@g.bracu.ac.bd>
2019-11-01 14:51:27 +0100

    This is a part of my python learning and currently updating the code from my python function learning.

--- funtion_practice.py
+++ funtion_practice.py
@@ -16,3 +16,33 @@ def lesser_of_two_evens(a,b):
         elif b<a:
             return a
     pass
+
+def animal_crackers(text):
+    '''
+
+    :param text: a rendom string with two words
+    :return: returns True if both words begin with same letter
+    '''
+    i = 0
+    limit = len(text)
+    for x in range (limit):
+        if text[x]==' ':
+            i = x
+    if text[0]==text[i+1]:
+         return True
+    else:
+        return False
+    pass
+
+def makes_twenty(n1,n2):
+    '''
+
+    :param n1: first integer
+    :param n2: second integer
+    :return:  return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
+    '''
+    if n1+n2==20 or n1==20 or n2==20:
+        return True
+    else:
+        return False
+    pass
